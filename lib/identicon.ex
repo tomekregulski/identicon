ba4399e4aa576adf3e5845ed985464dc97b8529e
Tomek Regulski <75456264+tomekregulski@users.noreply.github.com>
2021-04-16 12:02:26 -0400

    Add filter_odd_squares function

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -11,15 +11,33 @@ defmodule Identicon do
     |> hash_input
     |> pick_color
     |> build_grid
+    |> filter_odd_squares
+  end
+
+
+  @moduledoc """
+    Filter out odd squares so they do not get colored.
+  """
+  def filter_odd_squares(%Identicon.Image{grid: grid} = image) do
+    grid = Enum.filter grid, fn({code, _index}) -> 
+      rem(code, 2) == 0
+    end
+
+    %Identicon.Image{image | grid: grid}
   end
 
   @moduledoc """
     Builds the image grid and maps it with a reference to the helper function mirror_row
   """
   def build_grid(%Identicon.Image{hex: hex} = image) do
-    hex
-    |> Enum.chunk(3)
-    |> Enum.map(&mirror_row/1)
+    grid = 
+      hex
+      |> Enum.chunk(3)
+      |> Enum.map(&mirror_row/1)
+      |> List.flatten
+      |> Enum.with_index
+
+    %Identicon.Image{image | grid: grid}
   end
 
   @moduledoc """
